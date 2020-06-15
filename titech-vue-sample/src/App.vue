0df01ec4d58d29be2907d1af8242ddae4f817c1d
humminghorse <r.imahashi0724@gmail.com>
2020-06-16 01:11:28 +0900

    modify App.vue

--- titech-vue-sample/src/App.vue
+++ titech-vue-sample/src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
-    <h1>My Event</h1>
-    <p>Capacity: {{ capacity }}</p>
-    <button @click="increaseCapacity()">Increase Capacity</button>
+    <div id="app">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+    <div>
+      <h1>My Event</h1>
+      <p>Capacity: {{ capacity }}</p>
+      <button @click="increaseCapacity()">Increase Capacity</button>
+    </div>
   </div>
 </template>
 
 <script>
 import { ref } from "vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
   setup() {
     const capacity = ref(3);
 
@@ -20,3 +31,14 @@ export default {
   }
 };
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
